7b337eb2393edb60905b230ec4d436adb62cb3de
proXDhiya <prox.dhiya@gmail.com>
2024-08-24 03:05:09 +0100

    Chore(CI): add Test to github action #10

--- tests/__mock__/keys.mock.ts
+++ tests/__mock__/keys.mock.ts
@@ -3,5 +3,6 @@ import SharedStorage from "../../app/storage/SharedStorage";
 export const setupSharedStorage = () => {
     SharedStorage.set("key", "Hello World!");
     SharedStorage.set("REQ_name", "John Doe");
+    SharedStorage.set("REQ_country", "USA");
     SharedStorage.set("REQ_age", 25);
 };
